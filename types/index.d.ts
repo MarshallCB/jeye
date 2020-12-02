f7e03f371209ea4e6d6b29c2d2d9880cf30fe19d
Marshall Brandt <MarshallCB@users.noreply.github.com>
2020-12-01 19:39:19 -0600

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,6 @@ declare class Watcher{
   on(event: string, callback: (path: string, ...args: any) => void): Watcher
 }
 
-export declare function watch(source: string, options?: {ignore: RegExp, only: RegExp, chokidar: any}): Watcher;
+export declare function watch(source: string, options?: {ignore: RegExp, only: RegExp, chokidar: any, cache: any}): Watcher;
 
 export declare function targets(source: string, ignore?: RegExp, only?: RegExp): [{ rel: string, id: string }];
